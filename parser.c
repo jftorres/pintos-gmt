33dbb87118f498f0226d931a58cbb5457f8d3b94
jftorres <jftorres@caltech.edu>
2014-01-13 22:41:32 -0800

    Restructured the command struct, and added a command_list.
    Parser is still obviously not completed.

--- parser.c
+++ parser.c
@@ -3,44 +3,85 @@
 #define MAX_NUM_COMMANDS 16
 
 typedef struct command {
-    // The command to be executed.
-    char name[MAX_TOKEN_LEN + 1];
     // The arguments to the command.
+    // As is the convention, args[0] is the command name.
     char args[MAX_NUM_ARGS][MAX_TOKEN_LEN + 1];
     // The actual number of arguments provided.
     unsigned num_args;
-    // The actual lengths of the arguments.
-    unsigned[MAX_NUM_ARGS] arg_lens;
-    // The file descriptors for input/output.
-    int in_desc;
-    int out_desc;
-}
+} command;
+
+typedef struct command_list {
+    command *this;
+    command_list *next;
+} command_list;
     
 
-command * parse(char *input, int *num_commands) {
-    commmand *cmds = malloc(MAX_NUM_COMMANDS * sizeof(command));
-    char buf[MAX_NUM_ARGS][MAX_TOKEN_LEN+1];
-    
-    int num_args = 0;
+command_list parse(char *input) {
     int num_cmds = 0;
-    bool reading_args = 0;
+    // flag for if we're reading within quotes
+    bool quoted = 0;
+    // flag for reading text
+    bool reading = 0;
+    int fd[2];
 
-    unsigned i;
-    unsigned cmd_i = 0;
+    // set up the initial command and list
+    command *cmd = malloc(sizeof(command));
+    cmd->in_desc = 0;
+    cmd->out_desc = 1;
+    command_list *list_ptr = malloc(sizeof(command_list));
+    list_ptr->this = cmd;
+    list_ptr->next = 0;
+    command_list *head_ptr = list_ptr;
     for(i = 0; i < len; i++) {
         char curr = input[i];
         
-        switch(curr) {
+        if(curr == 0) {
             // reached the end
-            case 0:
-                
+
+            end_curr_argument(cmd);
+            // and ensure the list is terminated
+            cmd->next = 0;
+        } else if(curr == ' ' && !quoted) {
             // end the current argument, and set the flag to 0
-            case ' ':
-                if(reading_args) {
-                    num_args++;
-                    token = 0;
-                }
-                break;
-            case '|':
-                
+            if(reading) {
+                end_curr_argument(cmd)
+                reading = 0;
+            }
+        } else if(curr == '"') {
+            if(quoted) {
+                quoted = 0;
+                end_curr_argument(cmd);
+            } else {
+                quoted = 1;
+                reading = 1;
+            }
+        } else if(curr == '|' && !quoted) {
+            // end processing for the current command
+            if(reading) {
+                cmd->args[cmd->num_args][cmd->arg_lens[cmd->num_args]] = 0;
+                cmd->num_args++;
+                reading = 0;
+            }
+            
+            // set up the new command
+            cmd = malloc(sizeof(command));
+            
+            list_ptr->next = malloc(sizeof(command_list));
+            list_ptr = list_ptr->next;
+            list_ptr->this = cmd;
+            list_ptr->next = 0;
+        } else if(curr == '<' && !quoted) {
+            if(reading) {
+                cmd->args[cmd->num_args][cmd->arg_lens[cmd->num_args]] = 0;
+                cmd->num_args++;
+                reading = 0;
+            }
+            
+
+
+// null terminages the argument and counts it
+void end_curr_argument(command *cmd) {
+    cmd->args[cmd->num_args][cmd->arg_lens[cmd->num_args]] = 0;
+    cmd->num_args++;
+}
     
